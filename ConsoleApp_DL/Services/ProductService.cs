53c4734e1ae72d5ab127deed1008332e1a27773c
Eleonor <elidoescode@gmail.com>
2024-02-11 22:28:09 +0100

    Productservice komplett

--- ConsoleApp_DL/Services/ProductService.cs
+++ ConsoleApp_DL/Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleApp_DL.Repositories;
+﻿using ConsoleApp_DL.Entities;
+using ConsoleApp_DL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,50 @@ namespace ConsoleApp_DL.Services
         public ProductService(ProductRepository productRepository, CategoryService categoryService)
         {
             _productRepository = productRepository;
-            _categoryService = categoryService;
+            _categoryService = categoryService; 
         }
+
+
+        public ProductEntity CreateProduct(string title, decimal price, string categoryName)
+        {
+
+            var categoryEntity = _categoryService.CreateCategory(categoryName);
+
+            var productEntity = new ProductEntity
+            {
+                Title = title,
+                Price = price,
+                CategoryId = categoryEntity.Id,
+            };
+
+            productEntity = _productRepository.Create(productEntity);
+            return productEntity;
+        }
+
+
+
+        public ProductEntity GetProductById(int id)
+        {
+            var ProductEntity = _productRepository.Get(x => x.Id == id);
+            return ProductEntity;
+        }
+
+        public IEnumerable<ProductEntity> GetProducts()
+        {
+            var products = _productRepository.GetAll();
+            return products;
+        }
+
+        public ProductEntity UpdateProduct(ProductEntity ProductEntity)
+        {
+            var updatedProductEntity = _productRepository.Update(x => x.Id == ProductEntity.Id, ProductEntity);
+            return updatedProductEntity;
+        }
+
+        public void DeleteRole(int id)
+        {
+            _productRepository.Delete(x => x.Id == id);
+        }
+
     }
 }
