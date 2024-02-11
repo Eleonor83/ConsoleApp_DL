67ee544658fa21cbdb8f1388ee04990244237c21
Eleonor <elidoescode@gmail.com>
2024-02-11 23:08:17 +0100

    AddScoped i Program

--- ConsoleApp_DL/Program.cs
+++ ConsoleApp_DL/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ConsoleApp_DL.Contexts;
+using ConsoleApp_DL.Repositories;
+using ConsoleApp_DL.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,5 +9,18 @@ var builder = Host.CreateDefaultBuilder().ConfigureServices (services =>
 {
 
     services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\EC_Education\Datalagring\ConsoleApp_DL\ConsoleApp_DL\Data\database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True"));
+
+    services.AddScoped<AdressRepository>();
+    services.AddScoped<CategoryRepository>();
+    services.AddScoped<RoleRepository>();
+    services.AddScoped<ProductRepository>();
+    services.AddScoped<CustomerRepository>();
+
+    services.AddScoped<AdressService>();
+    services.AddScoped<CategoryService>();
+    services.AddScoped<RoleService>();
+    services.AddScoped<ProductService>();
+    services.AddScoped<CustomerService>();
 }
+
 ).Build ();
