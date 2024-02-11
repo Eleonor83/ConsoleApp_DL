ab4a8ddc6fda28ad69fd58d0c87d4153f562f016
Eleonor <elidoescode@gmail.com>
2024-02-11 23:19:36 +0100

    ConsoleUI

---
+++ ConsoleApp_DL/ConsoleUI.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp_DL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_DL
+{
+    internal class ConsoleUI
+    {
+        private readonly ProductService _productService;
+
+        public ConsoleUI(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public void CreateProduct_UI()
+        {
+            Console.Clear();
+            Console.WriteLine("----- CREATE PRODUCT -----");
+
+            Console.Write("Product Title: ");
+            var title = Console.ReadLine()!;
+
+            Console.Write("Product Price: ");
+            decimal price = decimal.Parse(Console.ReadLine()!);
+
+            Console.Write("Product Category: ");
+            var categoryName = Console.ReadLine()!;
+
+            var result = _productService.CreateProduct(title, price, categoryName);
+            if (result != null)
+            {
+                Console.Clear();
+                Console.WriteLine("Product was created.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
